ff6cd1c4dff0c1093d3c203871baebdaeec591a8
Youssef Kamel <120005163+YoussefKam@users.noreply.github.com>
2024-06-09 21:23:11 +0300

    Delete Client directory

--- Client/src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import AuthSlice from "./slices/AuthSlice";
-
-const rootReducer = combineReducers({
-  auth: AuthSlice,
-});
-
-export default rootReducer;
